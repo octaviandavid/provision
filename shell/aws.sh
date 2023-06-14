6c58a48549e1bd79173b38f74e938d22e3fc7f4d
Octavian David <2542748+octaviandavid@users.noreply.github.com>
2023-06-14 15:15:59 +0300

    preserve aws profile across sessions

--- shell/aws.sh
+++ shell/aws.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
-function aws_install {
+function install_awscli {
     cd $HOME
     mkdir -p ~/soft
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     ./aws/install --bin-dir ~/.local/bin --install-dir ~/soft/aws-cli --update
     rm -rf awscliv2.zip aws
-    ln -s ~/soft/aws-cli/v2/current/bin/aws ~/.local/bin/aws
+    ln -sf ~/soft/aws-cli/v2/current/bin/aws ~/.local/bin/aws
 } 
 
 # login
@@ -31,5 +31,11 @@ function ax {
     TEMP=$(aws configure list-profiles | fzf)
     [ ! -z "$TEMP" ] && export AWS_PROFILE=$TEMP ||  echo "please select a profile"
     echo "AWS_PROFILE is now $AWS_PROFILE"
+    echo $AWS_PROFILE > ~/.aws/.aws_profile
 }
+
+# load profile from file if it exists
+if [ -f ~/.aws/.aws_profile ]; then
+    export AWS_PROFILE=$(cat ~/.aws/.aws_profile)
+fi
 
